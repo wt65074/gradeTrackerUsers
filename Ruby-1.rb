017ccf7d2d03322ce2a2c4de655d1d0f408e6fc5
Will Tobey <wt65074@gmail.com>
2014-11-24 17:59:52 -0800

    first

---
+++ Ruby-1.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'mysql'
+require 'grape'
+require 'json'
+require 'rubygems'
+require 'open-uri'
+require 'net/http'
+
+
+module UserDataAPI
+    class V1 < Grape::API
+
+        version 'v1', using: :param, parameter: "v"
+        format :json
+
+        con = Mysql.new('127.0.0.1', 'root', '', 'UserData')
+
+        encoding_options = {
+          :invalid           => :replace,  # Replace invalid byte sequences
+          :undef             => :replace,  # Replace anything not defined in ASCII
+          :replace           => '',        # Use a blank for those replacements
+          :universal_newline => true       # Always break lines with \n
+        }
+
+        resource :weekly do
+          post do
+            
+            queryResponse = con.query("INSERT INTO weekly '#{params[:date]")
+            
+          end
+        end
+
+        resource :daily do
+          post do
+        
+            queryResponse = con.query("INSERT INTO daily '#{params[:date]")
+            
+          end
+        end
+
+
+      end
+end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './Ruby-1'
+
+run UserDataAPI::V1#!/usr/bin/env ruby
