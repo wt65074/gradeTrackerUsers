e814e8a1b0bb76e79f2445062846566b3584ea8d
wt65074 <wt65074@gmail.com>
2014-11-25 13:47:50 -0800

    Update Ruby-1.rb

--- Ruby-1.rb
+++ Ruby-1.rb
@@ -25,8 +25,8 @@ module UserDataAPI
         resource :weekly do
           post do
             
-            puts "INSERT INTO weekly (date) VALUES ('#{params[:date]}')"
-            queryResponse = con.query("INSERT INTO weekly (date) VALUES ('#{params[:date]}')")
+            puts "INSERT INTO Weekly (date) VALUES ('#{params[:date]}')"
+            queryResponse = con.query("INSERT INTO Weekly (date) VALUES ('#{params[:date]}')")
             
           end
         end
@@ -34,8 +34,8 @@ module UserDataAPI
         resource :daily do
           post do
             
-            puts "INSERT INTO weekly (date) VALUES ('#{params[:date]}')"
-            queryResponse = con.query("INSERT INTO weekly (date) VALUES ('#{params[:date]}')")
+            puts "INSERT INTO Weekly (date) VALUES ('#{params[:date]}')"
+            queryResponse = con.query("INSERT INTO Weekly (date) VALUES ('#{params[:date]}')")
             
           end
         end
