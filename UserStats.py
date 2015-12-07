fbfacd49140fb5f55a1f48983ab88d40f5f8dcb3
root <root@ip-172-31-20-31.us-west-1.compute.internal>
2015-12-07 19:12:11 +0000

    working user stats

--- UserStats.py
+++ UserStats.py
@@ -9,7 +9,7 @@ statsCursor = statsConnector.cursor()
 
 logsConnector = MySQLdb.connect(host="127.0.0.1",user="root",
             passwd="open",db="UserLogs")
-logsCursor = statsConnector.cursor()
+logsCursor = logsConnector.cursor()
 
 query = """SELECT date, count(*) as 'Count' FROM Daily GROUP BY date"""
 logsCursor.execute(query)
@@ -17,12 +17,22 @@ logsCursor.execute(query)
 #store query results
 data = logsCursor.fetchall()
 
-statsCursor.execute("""TRUNCATE TABLE DailyCount""")
+statsCursor.execute("""DROP TABLE DailyCount""")
+statsCursor.execute("""CREATE TABLE `DailyCount` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `count` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;""")
 
 for row in data:
 
-    query = "INSERT INTO DailyCount (date, count) VALUES (%s, %s)" % [row[0], row[1]]
+    query = """INSERT INTO DailyCount (date, count) VALUES ('%s', %s)""" % (row[0], row[1])
+    print query
     statsCursor.execute(query)
+
+logsConnector.commit()
+statsConnector.commit()
 
 logsCursor.close()
 statsCursor.close()
